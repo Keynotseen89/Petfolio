ef6774df54c41ac40ee212d3943f420cd2e28f7e
Keynotseen89 <ksintora@yahoo.com>
2017-01-04 18:03:26 -0800

    New Activity
    Added a new Activity named MedHistory

---
+++ app/src/main/java/com/example/kench/petfolio/MedHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.kench.petfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_med_history);
+    }
+}
