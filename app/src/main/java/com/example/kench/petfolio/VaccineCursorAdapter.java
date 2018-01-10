b92fca556b92c216cd23daf8cc16d75090b0b989
Keynotseen89 <ksintora@yahoo.com>
2018-01-09 23:19:03 -0800

    CursorAdapter
    Created a cursor adapter to populate the ListView.

---
+++ app/src/main/java/com/example/kench/petfolio/VaccineCursorAdapter.java
@@ -0,0 +1,45 @@
+package com.example.kench.petfolio;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.kench.petfolio.data.VaccineContract.VaccineEntry;
+
+/**
+ * Created by Quinatzin on 1/9/2018.
+ */
+
+public class VaccineCursorAdapter extends CursorAdapter {
+
+    public VaccineCursorAdapter(Context context, Cursor cursor){
+        super(context, cursor, 0 /* flags */);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        //Find the fields to populate in inflate template
+        TextView dateTextView = (TextView) view.findViewById(R.id.date_id);
+        TextView vaccTextView = (TextView) view.findViewById(R.id.vaccine_info_id);
+
+        //Find the columns of data that we're interested in
+        int dateColumnIndex = cursor.getColumnIndex(VaccineEntry.COLUMN_VACCINE_DATE);
+        int vacColumnIndex = cursor.getColumnIndex(VaccineEntry.COLUMN_VACCINE_INFO);
+
+        //Read the inventory attribute from the cursor for the current inventory
+        String date = cursor.getString(dateColumnIndex);
+        String vaccineInfo = cursor.getString(vacColumnIndex);
+
+        dateTextView.setText(date);
+        vaccTextView.setText(vaccineInfo);
+    }
+}
